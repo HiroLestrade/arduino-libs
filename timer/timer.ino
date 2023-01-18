0bb799c43a5cbeb7b04c2a11856ce4e9ed03284c
Hiro Lestrade <hles2078@gmail.com>
2023-01-18 00:38:24 -0600

    timer example
    First version of the timer example without implement the class.

--- timer/timer.ino
+++ timer/timer.ino
@@ -1,12 +1,28 @@
-//#include "timer.h"
-//Timer timer1(sampleTime);
+int led = 13;
+int ledState = HIGH;
 
-
-
-void loop(){
-
-}
+int sampleTime = 100;
+uint32_t currentTime  = 0;
+uint32_t previousTime = 0;
+uint32_t elapsedTime  = 0;
 
 void setup(){
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
+}
 
+void loop(){
+  
+}
+
+void setTimeOut(uint32_t sampleTime){
+  currentTime = millis();
+  elapsedTime = currentTime - previousTime;
+  if(elapsedTime > sampleTime){
+    //do something
+    ledState = !ledState;
+    digitalWrite(led, ledState);
+
+    previousTime = currentTime;
+  }
 }
